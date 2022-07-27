234cade5de76c679190c96973584ae5771576f53
Moon <89813511+Py-Moon@users.noreply.github.com>
2022-07-27 17:59:47 +0200

    Update ProxyScraper.py

--- ProxyScraper.py
+++ ProxyScraper.py
@@ -13,12 +13,14 @@ except ImportError:
 init(autoreset=True)
 class App:    
     def __init__(self) -> None:
+        print(f"{Fore.LIGHTGREEN_EX}ProxyScraper v{__VERSION__} by {__USER__}")
+        
         try: 
-            print(f"{Fore(Fore.BLUE)}Create PATH for Proxy List...")
+            print(f"{Fore.BLUE}Create PATH for Proxy List...")
             mkdir("ProxyLists")
-            print(f"{Fore(Fore.GREEN)}PATH CREATED SUCCESSFULLY!")
+            print(f"{Fore.GREEN}PATH CREATED SUCCESSFULLY!")
         except FileExistsError:
-            print(f"{Fore(Fore.GREEN)}PATH ALTREDY EXISTS")
+            print(f"{Fore.GREEN}PATH ALTREDY EXISTS")
         
         
         self.payload={}
@@ -28,9 +30,20 @@ class App:
         self.URLSOCKS4: str = "https://api.proxyscrape.com/v2/?request=displayproxies&proxytype=socks4&timeout=10000&country=all&ssl=all&anonymity=all"
         self.URLSOCKS5: str = "https://api.proxyscrape.com/v2/?request=displayproxies&proxytype=socks5&timeout=10000&country=all&ssl=all&anonymity=all"
         
-        print(self.GET_PROXYLIST())
-        print(f"{Fore(Fore.GREEN)} PROXY LIST CREATED SUCCESSFULLY!")
-           
+        self.GET_PROXYLIST()
+        print(f"{Fore.GREEN} PROXY LIST CREATED SUCCESSFULLY! ( AND SAVED IN FILE )")
+        
+        print(f"{Fore.LIGHTMAGENTA_EX} HTTP PROXY:\n {self.GET_HTTPPROXY()}")
+        print(f"{Fore.LIGHTYELLOW_EX} SOCKS4 PROXY:\n {self.GET_SOCKS4PROXY()}")
+        print(f"{Fore.LIGHTCYAN_EX} SOKS5 PROXY:\n {self.GET_SOCKS5PROXY()}")
+        
+        print(f"""
+{Fore.LIGHTMAGENTA_EX}COLOR HTTP PROXY: {Fore.LIGHTMAGENTA_EX}
+{Fore.LIGHTYELLOW_EX}COLOR SOCKS4 PROXY: {Fore.LIGHTYELLOW_EX}
+{Fore.LIGHTCYAN_EX}COLOR SOCKS5 PROXY: {Fore.LIGHTCYAN_EX}
+""")
+        print(f"{Fore.LIGHTGREEN_EX}ProxyScraper v{__VERSION__} by {__USER__}")
+
     def GET_HTTPPROXY(self) -> str:
         response = requests.request("GET", self.URLHTTP, headers=self.payload, data=self.payload)
         return response.text   
@@ -46,9 +59,9 @@ class App:
     
     def GET_PROXYLIST(self) -> list:
         list: list = []
-        list.append("HTTP PROXY:\n" + self.GET_HTTPPROXY() + "\n")
-        list.append("SOCKS4 PROXY:\n" + self.GET_SOCKS5PROXY() + "\n")
-        list.append("SOKS5 PROXY:\n" + self.GET_SOCKS5PROXY() + "\n")
+        list.append(f"{Fore.LIGHTMAGENTA_EX}HTTP PROXY:\n" + self.GET_HTTPPROXY() + "\n")
+        list.append(f"{Fore.LIGHTYELLOW_EX}SOCKS4 PROXY:\n" + self.GET_SOCKS5PROXY() + "\n")
+        list.append(f"{Fore.LIGHTCYAN_EX}SOKS5 PROXY:\n" + self.GET_SOCKS5PROXY() + "\n")
         
         with open("ProxyLists/ProxyList.txt", "w") as file:
             file.writelines(list)    
