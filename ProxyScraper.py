d6f6133f758b20621de5869e3998f45087205d06
Moon <89813511+Py-Moon@users.noreply.github.com>
2022-07-27 18:21:54 +0200

    Update ProxyScraper.py

--- ProxyScraper.py
+++ ProxyScraper.py
@@ -1,5 +1,5 @@
-__USER__: str  = "moonl1ght01"
-__VERSION__: str = "0.1"
+__USER__: str  = "moonl1ght01 AKA Py-Moon"
+__VERSION__: str = "0.5"
 __DATE__: str = "27-07-2022"
 
 from os import system
@@ -10,15 +10,17 @@ try:
     import requests
 except ImportError:
     system('python -m pip install colorama, requests')  
+    
 init(autoreset=True)
+
 class App:    
     def __init__(self) -> None:
-        print(f"{Fore.LIGHTGREEN_EX}ProxyScraper v{__VERSION__} by {__USER__}")
+        print(f"{Fore.RED}ProxyScraper v{__VERSION__} by {__USER__}")
         
         try: 
             print(f"{Fore.BLUE}Create PATH for Proxy List...")
-            mkdir("ProxyLists")
-            mkdir(r"ProxyLists/SortedProxyLists")
+            mkdir(r"ProxyLists/")
+            mkdir(r"ProxyLists/SortedProxyLists/")
             print(f"{Fore.GREEN}PATH CREATED SUCCESSFULLY!")
         except FileExistsError:
             print(f"{Fore.GREEN}PATH ALTREDY EXISTS")
@@ -28,31 +30,23 @@ class App:
         self.headers = {}     
         
         self.URLHTTP: str = "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all"
-        self.URLSOCKS4: str = "https://api.proxyscrape.com/v2/?request=displayproxies&proxytype=socks4&timeout=10000&country=all&ssl=all&anonymity=all"
         self.URLSOCKS5: str = "https://api.proxyscrape.com/v2/?request=displayproxies&proxytype=socks5&timeout=10000&country=all&ssl=all&anonymity=all"
         
         self.GET_PROXYLIST()
         print(f"{Fore.GREEN} PROXY LIST CREATED SUCCESSFULLY! ( AND SAVED IN FILE )")
         
         print(f"{Fore.LIGHTMAGENTA_EX} HTTP PROXY:\n {self.GET_HTTPPROXY()}")
-        print(f"{Fore.LIGHTYELLOW_EX} SOCKS4 PROXY:\n {self.GET_SOCKS4PROXY()}")
-        print(f"{Fore.LIGHTCYAN_EX} SOKS5 PROXY:\n {self.GET_SOCKS5PROXY()}")
+        print(f"{Fore.LIGHTYELLOW_EX} SOCKS4/5 PROXY:\n {self.GET_SOCKS4PROXY()}")
         
         print(f"""
 {Fore.LIGHTMAGENTA_EX}COLOR HTTP PROXY {Fore.LIGHTMAGENTA_EX}
-{Fore.LIGHTYELLOW_EX}COLOR SOCKS4 PROXY {Fore.LIGHTYELLOW_EX}
-{Fore.LIGHTCYAN_EX}COLOR SOCKS5 PROXY {Fore.LIGHTCYAN_EX}
+{Fore.LIGHTYELLOW_EX}COLOR SOCKS 4/5 PROXY {Fore.LIGHTYELLOW_EX}
 """)
-        print(f"{Fore.LIGHTGREEN_EX}ProxyScraper v{__VERSION__} by {__USER__}")
+        print(f"{Fore.RED}ProxyScraper v{__VERSION__} by {__USER__}")
 
     def GET_HTTPPROXY(self) -> str:
         response = requests.request("GET", self.URLHTTP, headers=self.payload, data=self.payload)
         return response.text   
-     
-    def GET_SOCKS4PROXY(self) -> str:
-
-        response = requests.request("GET", self.URLSOCKS4, headers=self.payload, data=self.payload)
-        return response.text    
     
     def GET_SOCKS5PROXY(self) -> str:
         response = requests.request("GET", self.URLSOCKS5, headers=self.payload, data=self.payload)
@@ -61,20 +55,17 @@ class App:
     def GET_PROXYLIST(self) -> list:
         list: list = []
         list.append(f"{Fore.LIGHTMAGENTA_EX}HTTP PROXY:\n" + self.GET_HTTPPROXY() + "\n")
-        list.append(f"{Fore.LIGHTYELLOW_EX}SOCKS4 PROXY:\n" + self.GET_SOCKS5PROXY() + "\n")
-        list.append(f"{Fore.LIGHTCYAN_EX}SOCKS5 PROXY:\n" + self.GET_SOCKS5PROXY() + "\n")
+        list.append(f"{Fore.LIGHTYELLOW_EX}SOCKS4/5 PROXY:\n" + self.GET_SOCKS5PROXY() + "\n")
         
         with open("ProxyLists/ProxyList.txt", "a") as file:
             file.writelines(list)
             file.close()
 
-        with open(r"ProxyLists/SortedProxyLists/HTTP_PROXY", "a") as file:
+        with open(r"ProxyLists/SortedProxyLists/HTTP_PROXY.txt", "a") as file:
             file.write(self.GET_HTTPPROXY())
             file.close()
-        with open(r"ProxyLists/SortedProxyLists/SOCKS4_PROXY", "a") as file:
-            file.write(self.GET_SOCKS4PROXY())
-            file.close()
-        with open(r"ProxyLists/SortedProxyLists/SOCKS5_PROXY", "a") as file:
+            
+        with open(r"ProxyLists/SortedProxyLists/SOCKS4-5_PROXY.txt", "a") as file:
             file.write(self.GET_SOCKS5PROXY())
             file.close()
         return list
